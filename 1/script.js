8f3bc0b3f4e677da5517ca9975536df63147a20d
tomhoon <gnsdl9108@naver.com>
2024-04-04 01:37:21 +0900

    feat: particles

--- 1/script.js
+++ 1/script.js
@@ -23,14 +23,31 @@ window.addEventListener('load', () => {
     console.log(ctx);
 
     class Particle {
-        construcoture() {
-
+        constructor(effect, x, y, color) {
+            this.effect = effect;
+            this.x = Math.random() * this.effect.canvasWidth;
+            this.y = 0;
+            this.color = color;
+            this.originX = x;
+            this.originY = y;
+            this.size = this.effect.gap - 1;
+            this.dx = 0;
+            this.dy = 0;
+            this.vx = 0;
+            this.vy = 0;
+            this.force = 0;
+            this.angle = 0;
+            this.distance = 0;
+            this.friction = Math.random() * 0.6 + 0.15;
+            this.ease = Math.random() * 0.1 + 0.005;
         }
         draw() {
-
+            this.effect.context.fillStyle = this.color;
+            this.effect.context.fillRect(this.x, this.y, this.size, this.size);
         }
         update() {
-
+            this.x += (this.originX - this.x) * this.ease;
+            this.y += (this.originY - this.y) * this.ease;
         }
     }
 
@@ -48,6 +65,17 @@ window.addEventListener('load', () => {
             this.textInput.addEventListener('keyup', (e) => {
                 this.context.clearRect(0,0,this.canvasWidth,this.canvasHeight);
                 this.wrapText(e.target.value);
+            });
+            this.particles = [];
+            this.gap = 5;
+            this.mouse = {
+                radius: 20000,
+                x: 0,
+                y: 0
+            }
+            window.addEventListener('mousemove', (e) => {
+                this.mouse.x = e.x;
+                this.mouse.y = e.y;
             });
         }
         wrapText(text) {
@@ -82,23 +110,50 @@ window.addEventListener('load', () => {
             linesArray.forEach((el, index) => {
                 this.context.fillText(el, this.textX, this.textY + (index * this.lineHeight));
                 this.context.strokeText(el, this.textX, this.textY + (index * this.lineHeight));
-            })
+            });
 
+            this.convertToParticles();
         }
         convertToParticles() {
-
+            this.particles = [];
+            const pixels = this.context.getImageData(0, 0, this.canvasWidth, this.canvasHeight).data;
+            this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+            // gap is how much jump to
+            // ref : gap.png
+            for (let y = 0; y < this.canvasHeight; y += this.gap){
+                for (let x = 0; x < this.canvasWidth; x += this.gap){
+                    const index = (y * this.canvasWidth + x) * 4;
+                    const alpha = pixels[index + 3];
+                    if (alpha > 0) {
+                        const red = pixels[index];
+                        const green = pixels[index + 1];
+                        const blue = pixels[index + 2];
+                        const color = `rgb( ${red}, ${green},${blue})`;
+                        this.particles.push(new Particle(this, x, y, color));
+                    }
+                }
+            }
+            // 하나당 픽셀 하나의 값을 의미함.
+            console.log(this.particles);
 
         }
         render() {
-
+            this.particles.forEach(particle => {
+                particle.update();
+                particle.draw();
+            })
         }
     }
 
 
     const effect = new Effect(ctx, canvas.width, canvas.height);
-    // effect.wrapText('hello my man');
-    // console.log('effect : ', effect);
-    function animate() {
+    effect.wrapText('똥꼬 보고싶어~~');
+    effect.render();
 
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        effect.render();
+        requestAnimationFrame(animate);
     }
+    animate();
 })
